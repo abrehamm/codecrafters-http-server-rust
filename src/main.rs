03f79f271eea9e55911403c21f1b006ad4e61c3f
Abreham M <abruki07@gmail.com>
2024-03-14 19:20:29 +0300

    Pass stage 2/8 - Respond with 200.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    println!("Listening on port: 4221");
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                stream
+                    .write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
+                    .unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);

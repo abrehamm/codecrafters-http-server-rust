fe2225c90444cd25aaa49779cb95b703228e6484
Abreham M <abruki07@gmail.com>
2024-03-22 17:08:30 +0300

    Pass stage 3/8 - Respond with 404.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::Write, net::TcpListener};
+use std::{io::Read, io::Write, net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -8,9 +8,24 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                stream
-                    .write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
-                    .unwrap();
+                let mut req = [0u8; 512];
+                let _ = stream.read(&mut req);
+                let req_str = &String::from_utf8(req.to_vec()).unwrap();
+                let req_vec: Vec<&str> = req_str.split("\r\n").collect();
+                println!("{}", req_str);
+                println!("{:?}", req_vec[0]);
+                let first_line: Vec<&str> = req_vec[0].split(' ').collect();
+                // let method = first_line[0];
+                let path = first_line[1];
+                if path == '/'.to_string() {
+                    stream
+                        .write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
+                        .unwrap();
+                } else {
+                    stream
+                        .write_all("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes())
+                        .unwrap();
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
